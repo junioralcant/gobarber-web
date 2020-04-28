47e2d714fde946336592169ce903937642212d5a
Junior Marques <junioralcant111@gmail.com>
2020-04-28 15:07:18 -0300

    creating validation singin and singup

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 // import { Container } from './styles';
 
 import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obriatório'),
+  password: Yup.string().required('A senha é brigatória'),
+});
 
 export default function SingIn() {
   function handlerSubmit(data) {
@@ -14,7 +22,7 @@ export default function SingIn() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handlerSubmit}>
+      <Form schema={schema} onSubmit={handlerSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
           name="password"
--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
-
+import * as Yup from 'yup';
 // import { Container } from './styles';
 
 import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obriatório'),
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obriatório'),
+  password: Yup.string()
+    .min(6, 'A senha precisa ter no mínimo 6 caracteres')
+    .required('A senha é brigatória'),
+});
 
 export default function SingUp() {
   function handlerSubimit(data) {
@@ -14,7 +24,7 @@ export default function SingUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handlerSubimit}>
+      <Form schema={schema} onSubmit={handlerSubimit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
