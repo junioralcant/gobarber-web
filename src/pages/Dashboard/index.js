4aee770617fbf92818cfcbc03bc32c94df57d280
Junior Marques <junioralcant111@gmail.com>
2020-05-05 17:19:05 -0300

    Authenticated requests

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import api from '../../services/api';
+
 export default function Dashboard() {
+  api.get('appointments');
   return <h1>Dashboard</h1>;
 }
